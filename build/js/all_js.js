86920f1a06021fa8a7fb73e6d1936eb3dab65f08
piaNINA <pianino4ka@gmail.com>
2022-11-24 00:58:57 +0200

    trip block

--- build/js/all_js.js
+++ build/js/all_js.js
@@ -5,4 +5,8 @@ $(document).ready(function () {
     $(this).toggleClass("burger-active");
     $(this).find($('.burger__line')).toggleClass('active');
   });
+  $('.balance__select').change(function () {
+    var iconUrl = $(this).find(':selected').attr('data-icon');
+    $(this).css('background', "url('".concat(iconUrl, "')"));
+  });
 });
--- src/js/script_1.js
+++ src/js/script_1.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
     $(this).toggleClass("burger-active");
     $(this).find($('.burger__line')).toggleClass('active')
   });
+  $('.balance__select').change(function () { 
+    const iconUrl = $(this).find(':selected').attr('data-icon'); 
+    $(this).css('background', `url('${iconUrl}')`); 
+  });
 });
